11f930c7ea72e6397da13ac3bf3c1c8b344e2a7c
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-03-16 00:24:59 +0900

    add a test for FormHandler.parseTSV

---
+++ test/form_handler_test.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var expect = require('chai').expect;
+var FormHandler = require('../lib/form_handler');
+
+describe('FormHandler', () => {
+    describe('parseTSV', ()=> {
+	it('expects to convert a TSV string into an array', () => {
+	    let tsv = "first\tsecond\tthird\r\n1st\t2nd\t3rd\r\n1\t2\t3\r\n";
+	    expect(FormHandler.parseTSV(tsv)).to.equal([
+		['first', 'second', 'third'],
+		['1st', '2nd', '3rd'],
+		['1', '2', '3']
+	    ]);
+	});
+    });
+});
+
