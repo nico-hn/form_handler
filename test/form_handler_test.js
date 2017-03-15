1a6838886e9c121aa27e7a74e32fa233c2fd86c2
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2017-03-16 00:50:15 +0900

    fix a test for FormHandler.parseTSV:
    "to.eql" should be used for testing nested arrays

--- test/form_handler_test.js
+++ test/form_handler_test.js
@@ -7,7 +7,7 @@ describe('FormHandler', () => {
     describe('parseTSV', ()=> {
 	it('expects to convert a TSV string into an array', () => {
 	    let tsv = "first\tsecond\tthird\r\n1st\t2nd\t3rd\r\n1\t2\t3\r\n";
-	    expect(FormHandler.parseTSV(tsv)).to.equal([
+	    expect(FormHandler.parseTSV(tsv)).to.eql([
 		['first', 'second', 'third'],
 		['1st', '2nd', '3rd'],
 		['1', '2', '3']
